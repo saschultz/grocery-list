cb91a50ddf2c7c1bd8d74da151bffbffde9edf12
Riley Watts and Sara Schultz <riley+sara@>
2017-02-20 17:13:26 -0800

    add to Upper Case and return result

--- js/scripts.js
+++ js/scripts.js
@@ -3,33 +3,14 @@ $(document).ready(function() {
     event.preventDefault();
     var inputs = ["grocery1", "grocery2", "grocery3", "grocery4"];
     var array = [];
-    var array2 = [];
-    inputs.map(function(input) {
+    inputs.forEach(function(input) {
       var list = $("input#" + input).val().toUpperCase();
       array.push(list);
-      array = array.sort(); //not 100% sure if this is correct
-      // return array.toUpperCase();
+      array.sort();
       });
-
-
-      // var array2 = array.map(function(caps) {
-      //   caps.toUpperCase();
-      //   // return caps;
-      // });
-
-
-      // var array3 = array2.sort();
-    console.log(array2);
+      array.forEach(function(answer) {
+        $("#result").append("<li>" + answer + "</li>")
+      });
+    console.log(array);
     });
   });
-
-
-
-
-
-
-// $("h1").click(function() {
-//
-//   var input = $("input#grocery").val();
-//   list.push(input);
-//   console.log(list);
